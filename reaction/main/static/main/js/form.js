a4633fd1efc6adb7d730d1c5dedf50675810d6ef
Volaner <myemail2004@inbox.ru>
2023-07-05 00:51:51 +0300

    Add autorization by AJAX

---
+++ reaction/main/static/main/js/form.js
@@ -0,0 +1,238 @@
+class Form
+{
+	#loginPage = ''
+	#loginAjaxUrl =  ''
+	#usernameLabel = 'Login'
+	#usernameIsinvalidLabel = 'Please enter your login'
+	#passwordLabel = 'Password'
+	#passwordIsinvalidLabel = 'Please enter your password'
+
+	constructor(urls)
+	{
+		this.#loginAjaxUrl = urls.loginAjax;
+		this.#loginPage = urls.loginPage;
+
+		let self = this;
+		let btn_signin_sample = document.querySelectorAll('button.sign_in_sample');
+		if(btn_signin_sample != null)
+		{
+	    	for (let btn of btn_signin_sample)
+	    	{
+	    		btn.addEventListener("click", function()
+				{
+		    		self.#pressSigninSampleBtn(btn);
+		    	});
+	    	}
+    	}
+
+    	let btn_signin = document.querySelectorAll('button.sign_in');
+		if(btn_signin != null)
+		{
+	    	for (let btn of btn_signin)
+	    	{
+	    		let inputs = [];
+	    		for (let elem of btn.closest('form').getElementsByTagName('input'))
+				{
+					if(elem instanceof HTMLInputElement)
+					{
+						elem.oninput = () =>
+						{
+							this.#validateInput(elem)
+						}
+
+						inputs[elem.name] = elem;
+					}
+				}
+
+	    		btn.addEventListener("click", function()
+				{
+		    		self.#pressSigninBtn(btn, inputs);
+		    	});
+
+		    	this.#checkRedirect(btn, inputs);
+	    	}
+    	}
+	}
+
+	#ajaxSend = async (url, formData) => {
+        const response = await fetch(url, {
+            method: "POST",
+            body: formData
+        });
+        if (!response.ok)
+        {
+            throw new Error('HTTP error, status: ' + response.status);
+        }
+        else
+        {
+        	return await response.json();
+        }
+    }
+
+    #validateInput(input)
+    {
+		if(input.value != '')
+		{
+			input.classList.remove('is-invalid');
+			if(input.name == 'username')
+			{
+				input.nextElementSibling.textContent = this.#usernameLabel	
+			}
+			else if(input.name == 'password')
+			{
+				input.nextElementSibling.textContent = this.#passwordLabel		
+			}								
+		}
+		else
+		{
+			input.classList.add('is-invalid');
+			if(input.name == 'username')
+			{
+				input.nextElementSibling.textContent = this.#usernameIsinvalidLabel
+			}
+			else if(input.name == 'password')
+			{
+				input.nextElementSibling.textContent = this.#passwordIsinvalidLabel
+			}
+		}
+    }
+
+	#pressSigninSampleBtn(btn)
+	{
+		let formData = new FormData(btn.closest('form'));
+
+		let username = formData.get('username');
+		let password = formData.get('password');
+
+		if(username != '' && password != '')
+		{
+			this.#ajaxSend(this.#loginAjaxUrl, formData)
+	            .then((response) =>
+	            {
+	            	if(response.status == 'Done')// User authorized
+	            	{
+	            		window.location.reload();
+	            	}
+	            	else if(response.status == 'Fail')// Invalid login or password
+	            	{
+	            		setCookie('authorizationStatus', 'Invalid login or password');
+
+	            		window.location.href = this.#loginPage;
+	            	}
+	            })
+	            .catch((err) => console.error(err))
+        }
+        else
+        {
+	        if(username == '' && password == '')
+			{
+				setCookie('authorizationStatus', 'username and password is empty');
+			}
+			else if(username == '')
+			{
+				setCookie('authorizationStatus', 'username is empty');
+			}
+			else
+			{
+				setCookie('authorizationStatus', 'password is empty');
+			}
+
+			window.location.href = this.#loginPage;
+		}
+	}
+
+	#pressSigninBtn(btn, inputs)
+	{
+		let form = btn.closest('form');
+		let formData = new FormData(form);
+
+		if(formData.get('username') != '' && formData.get('password') != '')
+		{
+			this.#ajaxSend(this.#loginAjaxUrl, formData)
+	            .then((response) =>
+	            {
+	            	if(response.status == 'Done')// User authorized
+	            	{
+	            		window.location.reload();
+	            	}
+	            	else if(response.status == 'Fail')// Invalid login or password
+	            	{
+	            		form.querySelector('div.form-error').textContent = 'Login or password is invalid'
+	            		inputs.username.classList.add('is-invalid');
+	            		inputs.password.classList.add('is-invalid');
+	            	}
+	            	else
+	            	{
+	            		console.log(response.status);
+	            	}
+	            })
+	            .catch((err) => console.error(err))
+		}
+		else
+		{
+			if(formData.get('username') == '')
+			{
+				inputs.username.classList.add('is-invalid');
+				inputs.username.nextElementSibling.textContent = this.#usernameIsinvalidLabel;
+			}
+			if(formData.get('password') == '')
+			{
+				inputs.password.classList.add('is-invalid');
+				inputs.password.nextElementSibling.textContent = this.#passwordIsinvalidLabel;
+			}
+		}
+	}
+
+	#checkRedirect(btn, inputs)
+	{
+		let status = getCookie('authorizationStatus');
+
+    	if(typeof(status) == 'undefined')
+    	{
+    		return;
+	    }
+	    else
+	    {
+	    	deleteCookie('authorizationStatus');
+	    }
+
+    	if(status == 'username and password is empty')
+    	{
+    		inputs.username.classList.add('is-invalid');
+    		inputs.password.classList.add('is-invalid');
+    		inputs.username.nextElementSibling.textContent = this.#usernameIsinvalidLabel;
+    		inputs.password.nextElementSibling.textContent = this.#passwordIsinvalidLabel;
+    	}
+    	else if(status == 'username is empty')
+    	{
+    		inputs.username.classList.add('is-invalid');
+    		inputs.username.nextElementSibling.textContent = this.#usernameIsinvalidLabel;
+    	}
+    	else if(status == 'password is empty')
+    	{
+    		inputs.password.classList.add('is-invalid');
+    		inputs.password.nextElementSibling.textContent = this.#passwordIsinvalidLabel;	
+    	}
+    	else if(status == 'Invalid login or password')
+    	{
+    		inputs.username.classList.add('is-invalid');
+    		inputs.password.classList.add('is-invalid');
+    		inputs.username.nextElementSibling.textContent = this.#usernameIsinvalidLabel;
+    		inputs.password.nextElementSibling.textContent = this.#passwordIsinvalidLabel;
+
+    		btn.closest('form').querySelector('div.form-error').textContent = 'Invalid login or password';
+    	}
+    	else
+    	{
+    		console.log('unknown status - ' + status);
+    	}
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function()
+{
+	new Form({
+		'loginAjax': '/login_ajax/',
+		'loginPage': '/login/'
+	});
+});
--- reaction/main/views.py
+++ reaction/main/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import logout, login
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import ListView, CreateView
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+import json
 
 from .forms import *
 from .models import *
@@ -65,6 +67,49 @@ class LoginUser(DataMixin, LoginView):
 
     def get_success_url(self):
         return reverse_lazy('home')
+
+
+# class LoginAjaxSample(View):
+#
+#     def post(self, request):
+#         post = json.loads(request.body)
+#         username = post.get('login')
+#         password = post.get('password')
+#
+#         if username and password:
+#             user = authenticate(username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return JsonResponse(data={'status': 'Done'}, status=200)
+#             else:
+#                 return JsonResponse(data={'status': 'Fail'}, status=200)
+#         else:
+#             return JsonResponse(data={'status': 'login or password is empty'}, status=200)
+
+
+class LoginAjax(View):
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return JsonResponse(data={'status': 'Done'}, status=200)
+            else:
+                return JsonResponse(data={'status': 'Fail'}, status=200)
+        elif username is None or password is None:
+            return JsonResponse(data={'status': 'Error: login or password is not exist'}, status=200)
+        elif username == '' and password == '':
+            return JsonResponse(data={'status': 'login and password is empty'}, status=200)
+        elif username == '':
+            return JsonResponse(data={'status': 'login is empty'}, status=200)
+        elif password == '':
+            return JsonResponse(data={'status': 'password is empty'}, status=200)
+        else:
+            return JsonResponse(data={'status': 'Error: something is wrong'}, status=200)
 
 
 def logout_user(request):
