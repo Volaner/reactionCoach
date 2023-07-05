df48f8306bceb182aaa49642d0f73d129cd0e1b5
Volaner <myemail2004@inbox.ru>
2023-07-05 23:25:20 +0300

    urls.py - removed login_ajax_sample

--- reaction/main/urls.py
+++ reaction/main/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', Index.as_view(), name='home'),
     path('records/', Records.as_view(), name='records'),
     path('login/', LoginUser.as_view(), name='login'),
-    # path('login_ajax_sample/', LoginAjaxSample.as_view(), name='loginAjaxSample'),
     path('login_ajax/', LoginAjax.as_view(), name='loginAjax'),
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
