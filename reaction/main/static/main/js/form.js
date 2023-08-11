2e2dd922553ddff6b9450d2c896bb1bb73a2fe39
Volaner <myemail2004@inbox.ru>
2023-08-11 20:48:15 +0300

    Forms - added frontend verification

--- reaction/main/static/main/js/form.js
+++ reaction/main/static/main/js/form.js
@@ -6,6 +6,15 @@ class Form
 	#usernameIsinvalidLabel = 'Please enter your login'
 	#passwordLabel = 'Password'
 	#passwordIsinvalidLabel = 'Please enter your password'
+	#usernameRegexp = /(.{3,})/
+	#usernameRegexpFailMessage = 'This usernam is too short. It must contain at least 3 characters.'
+	#emailRegexp = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/
+	#emailRegexpFailMessage = 'Enter a valid email address.'
+	#passwordRegexp = /((?=.*\d)(?=.*\D).{8,})/
+	#passwordRegexpFailMessage = 'Password must contain at least 8 characters and include letters and numbers.'
+	#signupBtnClicked = 0
+	#resetBtnClicked = 0
+	#resetConfirmBtnClicked = 0
 
 	constructor(urls)
 	{
@@ -13,10 +22,10 @@ class Form
 		this.#loginPage = urls.loginPage;
 
 		let self = this;
-		let btn_signin_sample = document.querySelectorAll('button.sign_in_sample');
-		if(btn_signin_sample != null)
+		let btns_signin_sample = document.querySelectorAll('button.sign_in_sample');
+		if(btns_signin_sample != null)
 		{
-	    	for (let btn of btn_signin_sample)
+	    	for (let btn of btns_signin_sample)
 	    	{
 	    		btn.addEventListener("click", function()
 				{
@@ -25,10 +34,10 @@ class Form
 	    	}
     	}
 
-    	let btn_signin = document.querySelectorAll('button.sign_in');
-		if(btn_signin != null)
+    	let btns_signin = document.querySelectorAll('button.sign_in');
+		if(btns_signin != null)
 		{
-	    	for (let btn of btn_signin)
+	    	for (let btn of btns_signin)
 	    	{
 	    		let inputs = [];
 	    		for (let elem of btn.closest('form').getElementsByTagName('input'))
@@ -53,27 +62,144 @@ class Form
 	    	}
     	}
 
+    	// Reseting password
+    	let btns_reset_password = document.querySelectorAll('button.reset_password');
 
-    	let reset_password = document.querySelectorAll('button.reset_password');
-		if(reset_password != null)
+		if(btns_reset_password != null)
 		{
-			for (let btn of reset_password)
+			for (let btn of btns_reset_password)
 	    	{
-	    		for (let elem of btn.closest('form').getElementsByTagName('input'))
+	    		btn.addEventListener("click", function()
+				{
+		    		self.#pressResetBtn(btn);
+		    	});
+
+	    		for (let input of btn.closest('form').getElementsByTagName('input'))
 	    		{
-	    			if(elem instanceof HTMLInputElement)
+	    			if(input.name == 'email')
 	    			{
-	    				elem.oninput = () =>
+	    				input.oninput = () =>
 						{
-							this.#validateInput(elem)
+							if(this.#resetBtnClicked)
+							{
+								this.#validateEmail(input)
+							}
 						}
 	    			}
 	    		}
+	    	}
+		}
+
+		// Confirm reset password
+		let btns_reset_password_confirm = document.querySelectorAll('button.reset_password_confirm');
+
+		if(btns_reset_password_confirm != null)
+		{
+			for (let btn of btns_reset_password_confirm)
+			{
+				let password1
+	    		let password2
+
+	    		self.#checkInputsValidationOnStartup(btn)
+
+				btn.addEventListener("click", function()
+				{
+		    		self.#pressResetConfirmBtn(btn);
+		    	});
+
+		    	// Bind validation inputs when user change input after first confirmResetBtn click
+	    		for (let input of btn.closest('form').getElementsByTagName('input'))
+				{
+					if(input.name == 'new_password1')
+					{
+						password1 = input
+					}
+					else if(input.name == 'new_password2')
+					{
+						password2 = input
+					}
+				}
+
+		    	password1.addEventListener('input', () => 
+				{
+					if(this.#resetConfirmBtnClicked)
+					{
+						this.#validatePassword(password1, password2)
+					}
+				})
+				password2.addEventListener('input', () => 
+				{
+					if(this.#resetConfirmBtnClicked)
+					{
+						this.#validatePassword(password1, password2)
+					}
+				})
+			}
+		}
+
+		// Registration
+		let btns_signup = document.querySelectorAll('button.sign_up');
+		if(btns_signup != null)
+		{
+			for (let btn of btns_signup)
+	    	{
+	    		let password1
+	    		let password2
+
+	    		self.#checkInputsValidationOnStartup(btn)
 
 	    		btn.addEventListener("click", function()
 				{
-		    		self.#pressResetBtn(btn, inputs);
+		    		self.#pressSignupBtn(btn);
 		    	});
+
+	    		// Bind validation inputs when user change input after first signupBtn click
+	    		for (let input of btn.closest('form').getElementsByTagName('input'))
+				{
+		    		if(input.name == 'username')
+					{
+						input.addEventListener('input', () => 
+						{
+							if(this.#signupBtnClicked)
+							{
+								this.#validateUsername(input)
+							}
+						})
+					}
+					else if(input.name == 'email')
+					{
+						input.addEventListener('input', () => 
+						{
+							if(this.#signupBtnClicked)
+							{
+								this.#validateEmail(input)
+							}
+						})
+					}
+					else if(input.name == 'password1')
+					{
+						password1 = input
+					}
+					else if(input.name == 'password2')
+					{
+						password2 = input
+					}
+				}
+
+		    	password1.addEventListener('input', () => 
+				{
+					if(this.#signupBtnClicked)
+					{
+						this.#validatePassword(password1, password2)
+					}
+				})
+				password2.addEventListener('input', () => 
+				{
+					if(this.#signupBtnClicked)
+					{
+						this.#validatePassword(password1, password2)
+					}
+				})
 	    	}
 		}
 	}
@@ -209,7 +335,99 @@ class Form
 
 	#pressResetBtn(btn)
 	{
-		console.log(btn)
+		let form = btn.closest('form')
+
+		this.#resetBtnClicked = 1
+
+		for (let input of form.getElementsByTagName('input'))
+		{
+			if(input.name == 'email')
+			{
+				if(this.#validateEmail(input))
+				{
+					form.submit()
+				}
+			}
+		}
+	}
+
+	#pressResetConfirmBtn(btn)
+	{
+		let isInvalid = false
+		let form = btn.closest('form')
+		let password1
+		let password2
+
+		this.#resetConfirmBtnClicked = 1
+
+		for (let input of form.getElementsByTagName('input'))
+		{
+			if(input.name == 'new_password1')
+			{
+				password1 = input
+			}
+			else if(input.name == 'new_password2')
+			{
+				password2 = input
+			}
+		}
+
+		if((typeof(password1) != 'undefined' && password1 !== null) && (typeof(password2) != 'undefined' && password2 !== null))
+		{
+		    if(this.#validatePassword(password1, password2))
+			{
+				form.submit()
+			}
+		}
+	}
+
+	#pressSignupBtn(btn)
+	{
+		let isInvalid = false
+		let form = btn.closest('form')
+		let password1
+		let password2
+
+		this.#signupBtnClicked = 1
+
+		for (let input of form.getElementsByTagName('input'))
+		{
+			if(input.name == 'username')
+			{
+				if(!this.#validateUsername(input))
+				{
+					isInvalid = true;
+				}
+			}
+			else if(input.name == 'email')
+			{
+				if(!this.#validateEmail(input))
+				{
+					isInvalid = true;
+				}
+			}
+			else if(input.name == 'password1')
+			{
+				password1 = input
+			}
+			else if(input.name == 'password2')
+			{
+				password2 = input
+			}
+		}
+
+		if((typeof(password1) != 'undefined' && password1 !== null) && (typeof(password2) != 'undefined' && password2 !== null))
+		{
+		    if(!this.#validatePassword(password1, password2))
+			{
+				isInvalid = true;
+			}
+		}
+
+		if(!isInvalid)
+		{
+			form.submit()
+		}
 	}
 
 	#checkRedirect(btn, inputs)
@@ -255,6 +473,128 @@ class Form
     	{
     		console.log('unknown status - ' + status);
     	}
+	}
+
+	#checkInputsValidationOnStartup(btn)
+	{
+		for (let div of btn.closest('form').querySelectorAll('div.div-input'))
+		{
+			if(div.getAttribute('data-haserror') == '1')
+			{
+				let invalid_feedback_div = div.querySelector('div.invalid-feedback')
+				let ul = invalid_feedback_div.querySelector('ul')
+				let message = ul.querySelector('li').textContent
+
+				div.querySelector('input').classList.add('is-invalid')
+				ul.remove()
+				invalid_feedback_div.textContent = message
+			}
+		}
+	}
+
+	#validateUsername(input)
+	{
+		let feedback_input = input.closest('div').querySelector('div.invalid-feedback')
+
+		if(input.value == '')
+		{
+			feedback_input.textContent = 'This field is required.'
+			input.classList.add('is-invalid')
+
+			return false
+		}
+		else if(input.value.match(this.#usernameRegexp) === null)
+		{
+			feedback_input.textContent = this.#usernameRegexpFailMessage
+			input.classList.add('is-invalid')
+
+			return false
+		}
+		else
+		{
+			input.classList.remove('is-invalid')
+
+			return true
+		}
+	}
+
+	#validateEmail(input)
+	{
+		let feedback_input = input.closest('div').querySelector('div.invalid-feedback')
+
+		if(input.value == '')
+		{
+			feedback_input.textContent = 'This field is required.'
+			input.classList.add('is-invalid')
+
+			return false
+		}
+		if(input.value.match(this.#emailRegexp) === null)
+		{
+			feedback_input.textContent = this.#emailRegexpFailMessage
+			input.classList.add('is-invalid')
+
+			return false
+		}
+		else
+		{
+			input.classList.remove('is-invalid')
+
+			return true
+		}
+	}
+
+	#validatePassword(password1, password2)
+	{
+		let feedback_input = password2.closest('div').querySelector('div.invalid-feedback')
+		let result
+
+		if(password1.value == '')
+		{
+			password1.closest('div').querySelector('div.invalid-feedback').textContent = 'This field is required.'
+			password1.classList.add('is-invalid')
+
+			result = false
+		}
+		else if(password2.value == '')
+		{
+			feedback_input.textContent = 'This field is required.'
+			password2.classList.add('is-invalid')
+
+			result = false
+		}
+		else if(password1.value != password2.value)
+		{
+			feedback_input.textContent = 'The two password fields didn’t match.';
+			password1.classList.add('is-invalid');
+			password2.classList.add('is-invalid');
+
+			result = false
+		}
+		else if(password1.value.match(this.#passwordRegexp) === null)
+		{
+			feedback_input.textContent = this.#passwordRegexpFailMessage
+			password1.classList.add('is-invalid')
+			password2.classList.add('is-invalid')
+
+			result = false
+		}
+		else
+		{
+			password1.classList.remove('is-invalid')
+			password2.classList.remove('is-invalid')
+
+			result = true
+		}
+
+		if(result)
+		{
+			return true
+		}
+		else
+		{
+
+		}
 	}
 }
 
